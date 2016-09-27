327ab6bbb8f0b588745eaa86e851291534731218
gabrielamelian <gabrielamelian@gmail.com>
2016-09-28 10:36:27 +1300

    Add jQuery code for Ajax

--- main.js
+++ main.js
@@ -15,3 +15,23 @@ function quoteIt () {
     alert("I want a new quote!");
 }
 
+function getQuote() {
+    $.ajax({
+   url: 'http://quotes.rest/qod.json',
+   data: {
+      format: 'json'
+   },
+   error: function() {
+      $('#quote').html('<p>An error has occurred</p>');
+   },
+   dataType: 'jsonp',
+   success: function(data) {
+      var $quote = $('<h1>').text(contents.quotes[0].quote);
+      var $author = $('<p>').text(contents.quotes[0].author);
+      $('#quote')
+         .append($quote)
+         .append($author);
+   },
+   type: 'GET'
+});
+}
